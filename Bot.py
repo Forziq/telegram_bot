c23ed86ff955bf3d9a9633f905d80ec9127f9c5a
Forzammil <forzammil@gmail.com>
2023-02-11 16:23:41 +0000

    openai_key

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import openai
+openai.api_key = "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"
 from aiogram import Bot,types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
